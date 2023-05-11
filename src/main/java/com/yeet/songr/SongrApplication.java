5f753b55084f224cac2010339e30920970707298
Christopher Foster <cfosprof@gmail.com>
2023-05-11 05:10:02 -0700

    Completed lab 11

--- src/main/java/com/yeet/songr/SongrApplication.java
+++ src/main/java/com/yeet/songr/SongrApplication.java
@@ -3,11 +3,12 @@ package com.yeet.songr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Main entry point for the Spring Boot application
 @SpringBootApplication
 public class SongrApplication {
 
+	// Main method to start the application
 	public static void main(String[] args) {
 		SpringApplication.run(SongrApplication.class, args);
 	}
-
 }
